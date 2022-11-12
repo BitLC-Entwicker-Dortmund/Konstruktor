4f814bc4c3a81bf611908836d910118f5d548875
k s <klaus.stolz@web.de>
2022-11-12 13:15:13 +0100

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konstruktor {
+    class Person {
+        private string vorname;
+        private string nachname;
+
+        // Standardkonstruktor - manipuliert
+        public Person ( ) {
+            vorname = "James";
+            nachname = "Blond";
+        }
+
+        
+        // überladener Konstruktor
+        public Person (string vorname, string nachname ) {
+            this.vorname = vorname;
+            this.nachname = nachname;
+        }
+
+        public void ZeigMal ( ) {
+            Console.WriteLine ("Vorname: {0} Nachname: {1}", vorname, nachname);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konstruktor {
+    class Program {
+        static void Main ( string [ ] args ) {
+            Person robert = new Person ( );
+            robert.ZeigMal ( );
+
+            Person p = new Person ("Sebastian", "Boo" );
+            p.ZeigMal ( );
+
+            Console.Read ( );
+        }
+    }
+}
